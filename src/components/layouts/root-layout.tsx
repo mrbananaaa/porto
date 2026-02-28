faaef58fbd5f826e2f477f43fc55354c6270d039
mrbananaaa <mailme.mrbananaaaid@gmail.com>
2026-02-28 20:40:20 +0700

    fix navbar padding bug

--- src/components/layouts/root-layout.tsx
+++ src/components/layouts/root-layout.tsx
@@ -3,13 +3,13 @@ import { Navbar } from "@/components/common/navbar";
 
 export const RootLayout = () => {
   return (
-    <>
+    <main className="py-4">
       <Navbar />
       <div className="flex justify-center min-h-screen w-full bg-background-light">
-        <main className="px-4 md:px-8 lg:px-0 lg:max-w-4xl xl:max-w-6xl">
+        <div className="px-4 md:px-8 lg:px-0 lg:max-w-4xl xl:max-w-6xl">
           <Outlet />
-        </main>
+        </div>
       </div>
-    </>
+    </main>
   );
 };
